addd7ca8c48946fd5b396db3443b04a47863271c
Server <hydragen3295@yahoo.co.jp>
2020-10-13 16:37:09 +0900

    テスト起動結果を反映

--- start.sh
+++ start.sh
@@ -2,10 +2,5 @@
 cd /usr/local/bin/PaperMCserver/
 . ./var.sh
 #変数取り込み
-if [ 1 -eq `screen -ls | grep "${scr}" | wc -l` ] ; then
-  logger -s -p user.err -t [server] Already PaperMC server started.
-  exit 1 #多重起動防止
-else
-  screen -AmdS ${scr} java -server -Xms${mem} -Xmx${mem} -jar paper-${ver}.jar nogui #通常起動
-fi
-logger -s -p user.info -t [server] PaperMC server started. You can use 'screen -r ${scr}' to access console.
+screen -Dm -S ${scr} java -Xms${mem} -Xmx${mem} -jar paper-${ver}.jar
+echo "PaperMC server started. You can use screen -r ${scr} to access console."
